2dbfd926a2b6909d04b596d68486027d653a704b
rurakite <ruravictor@gmail.com>
2023-07-08 11:20:26 +0000

    - Created the dashboard page with appropriate navigation for easy navigation within the application - Implemented custom CSS to display the sidebar on larger screens, improving the user experience - Added a quote card component to display inspiring quotes on the dashboard - Integrated a weather widget to provide current weather information to users

--- habitrain/urls.py
+++ habitrain/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('register', views.register, name="register"),
     path('login', views.login, name="login"),
     path('user-logout', views.user_logout, name="user-logout"),
+    path('dashboard', views.dashboard, name="dashboard"),
     
 ]
--- habitrain/views.py
+++ habitrain/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Habit, Daily, Profile, User
 from .forms import (
     CreateUserForm,
@@ -8,6 +8,10 @@ from .forms import (
     CreateHabitForm,
     CreateDailyForm,
 )
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from datetime import date, timedelta
 
 
 
@@ -58,3 +62,29 @@ def user_logout(request):
     auth.logout(request)
 
     return redirect("")
+
+
+@login_required(login_url="login")
+def dashboard(request):
+    current_date = date.today()
+    week = current_date.isocalendar()[1]
+    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    start_current_week = current_date - timedelta(days=current_date.weekday())
+    end_current_week = start_current_week + timedelta(days=6)
+
+    week_dates = []
+    for i in range(7):
+        day = start_current_week + timedelta(days=i)
+        week_dates.append({'day_of_week': days[i], 'date': day})
+    habits = Habit.objects.all()
+    dailies = Daily.objects.all()
+
+    context = {
+        "week": week,
+        'week_dates': week_dates,
+        "current_date": current_date,
+        'habits': habits,
+        'dailies': dailies,
+        'days': days,
+    }
+    return render(request, "profile/dashboard.html", context)
