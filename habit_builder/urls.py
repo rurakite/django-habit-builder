6932dfc82c2830b5c567236c5197691d3a563090
rurakite <ruravictor@gmail.com>
2023-07-13 21:39:15 +0000

    Add error handling functionality

--- habit_builder/urls.py
+++ habit_builder/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500, handler400, handler403
+
+handler404 = 'habitrain.views.handler404'
+handler500 = 'habitrain.views.handler500'
+handler400 = 'habitrain.views.handler400'
+handler403 = 'habitrain.views.handler403'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- habitrain/views.py
+++ habitrain/views.py
@@ -294,10 +294,25 @@ def statistics(request):
     return render(request, "profile/statistics.html", context=context)
 
 
-
-
+@login_required(login_url="login")
 def view_habits_cards(request):
     habits = Habit.objects.filter(user=request.user)
 
     context = {"habits": habits}
     return render(request, "profile/view-habits-cards.html", context=context)
+
+
+def handler400(request, exception):
+    return render(request, 'error/400.html', status=400)
+
+
+def handler403(request, exception):
+    return render(request, 'error/403.html', status=403)
+
+
+def handler404(request, exception):
+    return render(request, 'error/404.html', status=404)
+
+
+def handler500(request):
+    return render(request, 'error/500.html', status=500)
